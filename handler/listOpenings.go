eb11211a22ba0981c13db4802272858307d8e436
febrandelli <BYH@8386ice>
2024-07-12 16:35:43 -0300

    feat: create endpoint to return all oponings or by id

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -1,12 +1,17 @@
 package handler
 
 import (
+	"github.com/febrandelli/gojobs/schema"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ListOpeningsHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "GET listing all openings",
-	})
+	var openings []schema.Opening
+	if err := db.Find(&openings).Error; err != nil {
+		logger.Errorf("error finding openings: %v", err)
+		sendErrorResponse(c, http.StatusInternalServerError, "error finding openings")
+	}
+
+	sendSuccessResponse(c, "list-openings", openings)
 }
--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,12 +1,27 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/febrandelli/gojobs/schema"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ShowOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "GET showing a opening",
-	})
+	id := c.Param("id")
+	if id == "" {
+		logger.Errorf("id is required")
+		sendErrorResponse(c, http.StatusBadRequest, "id is required")
+		return
+	}
+
+	var opening schema.Opening
+
+	if err := db.First(&opening, id).Error; err != nil {
+		logger.Errorf("opening id: %v notfound", err)
+		sendErrorResponse(c, http.StatusNotFound, fmt.Sprintf("opening id: %s not found", id))
+		return
+	}
+
+	sendSuccessResponse(c, "show-opening", opening)
 }
