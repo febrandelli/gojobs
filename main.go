66a72d6df3b06bc1bcb69022f4da0e15aa1cf70b
febrandelli <BYH@8386ice>
2024-07-11 22:32:01 -0300

    feat: adding logger and initialize config

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/febrandelli/gojobs/router"
+import (
+	"github.com/febrandelli/gojobs/config"
+	"github.com/febrandelli/gojobs/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Initialize()
+	if err != nil {
+		logger.Errorf("Error initializing config, error: %v", err)
+		return
+	}
 	router.Initialize()
 }
